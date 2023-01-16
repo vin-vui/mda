94d9671bd760fe32ad0a46d36d3c17f9d784e37b
vinvui <vincent.vuillemin@gmail.com>
2023-01-16 14:22:10 +0100

    Add Plausible to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="px-4 mx-auto sm:px-0">
-                    <iframe plausible-embed src="https://plausible.io/share/parcaventure-malbuisson.fr?auth=eHB-pzNDnym1DQ3XtIJSU&embed=true&theme=light" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+                    <iframe plausible-embed src="https://plausible.io/share/project-mda.vinvui.com?auth=D8yBBYgfg4ImLT6F1Vfrv&embed=true&theme=light" scrolling="no" frameborder="0" loading="lazy" style="width: 1px; min-width: 100%; height: 1600px;"></iframe>
+                    <div style="font-size: 14px; padding-bottom: 14px;">Stats powered by <a target="_blank" style="color: #4F46E5; text-decoration: underline;" href="https://plausible.io">Plausible Analytics</a></div>
                     <script async src="https://plausible.io/js/embed.host.js"></script>
                 </div>
             </div>
