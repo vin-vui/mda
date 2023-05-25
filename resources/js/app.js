95e746d21dd278659a3ff1cc89822c9ef19c0b42
vinvui <admin@vinvui.com>
2023-05-25 10:18:30 +0200

    Hotfix #9

--- resources/js/app.js
+++ resources/js/app.js
@@ -34,123 +34,43 @@ document.addEventListener('DOMContentLoaded', () => {
     const fileInput6 = document.getElementById('input_6');
 
     if (fileInput1) {
-        fileInput1.addEventListener('change', () => {
-            if (fileInput1.files.length > 0) {
-                const fileSize = fileInput1.files.item(0).size;
-                const fileMb = fileSize / 1024 ** 2;
-                if (fileMb >= 2) {
-                    alert('Merci de téléverser des images de moins de 2MB');
-                    window.location.reload();
-                } else {
-                    let image = document.getElementById('image_1');
-                    initializeCropper(image);
-                    document.getElementById('sendCroppedImage1').addEventListener('click', () => {
-                        const croppedCanvas = cropper.getCroppedCanvas();
-                        const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_1', croppedImageDataUrl);
-                    });
-                }
-            }
-        });
+        checkAndCrop(fileInput1, 'image_1', 'sendCroppedImage1');
     }
     if (fileInput2) {
-        fileInput2.addEventListener('change', () => {
-            if (fileInput2.files.length > 0) {
-                const fileSize = fileInput2.files.item(0).size;
-                const fileMb = fileSize / 1024 ** 2;
-                if (fileMb >= 2) {
-                    alert('Merci de téléverser des images de moins de 2MB');
-                    window.location.reload();
-                } else {
-                    let image = document.getElementById('image_2');
-                    initializeCropper(image);
-                    document.getElementById('sendCroppedImage2').addEventListener('click', () => {
-                        const croppedCanvas = cropper.getCroppedCanvas();
-                        const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_2', croppedImageDataUrl);
-                    });
-                }
-            }
-        });
+        checkAndCrop(fileInput2, 'image_2', 'sendCroppedImage2');
     }
     if (fileInput3) {
-        fileInput3.addEventListener('change', () => {
-            if (fileInput3.files.length > 0) {
-                const fileSize = fileInput3.files.item(0).size;
+        checkAndCrop(fileInput3, 'image_3', 'sendCroppedImage3');
+    }
+    if (fileInput4) {
+        checkAndCrop(fileInput4, 'image_4', 'sendCroppedImage4');
+    }
+    if (fileInput5) {
+        checkAndCrop(fileInput5, 'image_5', 'sendCroppedImage5');
+    }
+    if (fileInput6) {
+        checkAndCrop(fileInput6, 'image_6', 'sendCroppedImage6');
+    }
+
+    function checkAndCrop(fileInput, name, id) {
+
+        fileInput.addEventListener('change', () => {
+            if (fileInput.files.length > 0) {
+                const fileSize = fileInput.files.item(0).size;
                 const fileMb = fileSize / 1024 ** 2;
                 if (fileMb >= 2) {
                     alert('Merci de téléverser des images de moins de 2MB');
                     window.location.reload();
                 } else {
-                    let image = document.getElementById('image_3');
+                    let image = document.getElementById(name);
                     initializeCropper(image);
-                    document.getElementById('sendCroppedImage3').addEventListener('click', () => {
+                    document.getElementById(id).addEventListener('click', () => {
                         const croppedCanvas = cropper.getCroppedCanvas();
                         const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_3', croppedImageDataUrl);
+                        Livewire.emit('uploadCroppedImage', name, croppedImageDataUrl);
                     });
                 }
             }
         });
-    }
-    if (fileInput4) {
-        if (fileInput4.files.length > 0) {
-            const fileSize = fileInput4.files.item(0).size;
-            const fileMb = fileSize / 1024 ** 2;
-            if (fileMb >= 2) {
-                alert('Merci de téléverser des images de moins de 2MB');
-                window.location.reload();
-            } else {
-                fileInput4.addEventListener('change', () => {
-                    let image = document.getElementById('image_4');
-                    initializeCropper(image);
-                    document.getElementById('sendCroppedImage4').addEventListener('click', () => {
-                        const croppedCanvas = cropper.getCroppedCanvas();
-                        const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_4', croppedImageDataUrl);
-                    });
-                });
-            }
-        }
-    }
-    if (fileInput5) {
-        if (fileInput5.files.length > 0) {
-            const fileSize = fileInput5.files.item(0).size;
-            const fileMb = fileSize / 1024 ** 2;
-            if (fileMb >= 2) {
-                alert('Merci de téléverser des images de moins de 2MB');
-                window.location.reload();
-            } else {
-                fileInput5.addEventListener('change', () => {
-                    let image = document.getElementById('image_5');
-                    initializeCropper(image);
-                    document.getElementById('sendCroppedImage5').addEventListener('click', () => {
-                        const croppedCanvas = cropper.getCroppedCanvas();
-                        const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_5', croppedImageDataUrl);
-                    });
-                });
-            }
-        }
-    }
-    if (fileInput6) {
-        if (fileInput6.files.length > 0) {
-            const fileSize = fileInput6.files.item(0).size;
-            const fileMb = fileSize / 1024 ** 2;
-            if (fileMb >= 2) {
-                alert('Merci de téléverser des images de moins de 2MB');
-                window.location.reload();
-            } else {
-                fileInput6.addEventListener('change', () => {
-                    let image = document.getElementById('image_6');
-                    initializeCropper(image);
-                    document.getElementById('sendCroppedImage6').addEventListener('click', () => {
-                        const croppedCanvas = cropper.getCroppedCanvas();
-                        const croppedImageDataUrl = croppedCanvas.toDataURL();
-                        Livewire.emit('uploadCroppedImage', 'image_6', croppedImageDataUrl);
-                    });
-                });
-            }
-        }
-    }
+    };
 });
